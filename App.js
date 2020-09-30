2bb9052dfbf144edbb94c6d4aa34e89b1787293c
GeraldoJAlves <junior.alves.2007@gmail.com>
2020-09-29 21:08:40 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {View, Text, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View>
+        <Text>Simple Chat</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
